cdcadeb0523eebe941df50d971d43fb6c41dc42f
lananek <116422832+lananek@users.noreply.github.com>
2022-12-30 18:19:38 +0300

    Update isPrimeWithThreads.cpp

--- isPrimeWithThreads.cpp
+++ isPrimeWithThreads.cpp
@@ -6,7 +6,7 @@
 using namespace std;
 
 bool isPrime(int num) {
-	if (num == 0) 
+	if (num == 0 || num == 1) 
 		return false;
 	for (int i = 2; i * i <= num; i++) {
 		if (num % i == 0) return false;
